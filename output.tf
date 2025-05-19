ed835faeffea331cc3a5b6569378c3ce092aa4c2
Devendra Kulkarni <devendrakulkarni138057@gmail.com>
2025-05-19 11:47:53 +0530

    modified output

--- output.tf
+++ output.tf
@@ -5,9 +5,19 @@ output "instance_public_ip" {
 
 output "kubeconfig_path" {
   description = "Path to the kubeconfig file"
-  value       = module.rke2_node.kubeconfig_path
+  value       = local_file.kube_config_yaml.filename
 }
 
-#output "elastic_ip" {
-#  value = module.rke2_node.ec2_eip.public_ip
-#}
+output "next_steps" {
+  description = "Follow these steps for accessing SUSE AI"
+  value       = <<EOT
+  To access SUSE AI WebUI interface through your web browser, 
+  add below entry in your system's /etc/hosts file:
+  
+  <PUBLIC_IP_OF_EC2_INSTANCE>  suse-ollama-webui 
+
+  And then access via https://suse-ollama-webui
+
+  You should see a singup/login page, please signup for the first user, this user will have the admin privileges.
+  EOT
+}
