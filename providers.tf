27c9c82cf99c054ddf505814ed2b875ff3ec6805
Peter <18663039+petersatsuse@users.noreply.github.com>
2025-09-10 18:11:07 +0200

    added attitional providers

--- providers.tf
+++ providers.tf
@@ -1,12 +1,36 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
     ssh = {
       source  = "loafoe/ssh"
-      version = "2.7.0"
+      version = "~> 2.6"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
     }
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 provider "aws" {
-  region = var.aws_region
+  region = var.region
 }
