2012209886b24115627e0e2498ee47364329fee5
Peter <18663039+petersatsuse@users.noreply.github.com>
2025-09-10 17:19:44 +0200

    refactor aws output.tf to new structur

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -1,12 +1,14 @@
-output "ec2_public_ip" {
+output "public_ip" {
   value = aws_eip.ec2_eip.public_ip
 }
 
-output "kubeconfig_path" {
-  value = "${path.root}/modules/infrastructure/kubeconfig-rke2.yaml"
+output "ssh_private_key_content" {
+  description = "Content of the SSH private key."
+  value       = var.use_existing_ssh_public_key ? var.user_ssh_private_key : one(tls_private_key.ssh_keypair[*]).private_key_openssh
+  sensitive   = true
 }
 
-output "ssh_private_key_content" {
-  value     = var.use_existing_ssh_public_key ? data.local_file.ssh_private_key[0].content : tls_private_key.ssh_keypair[0].private_key_openssh
-  sensitive = true
+output "ssh_user" {
+  description = "The SSH user for the created VM."
+  value       = var.ssh_user
 }
