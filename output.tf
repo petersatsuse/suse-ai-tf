5d85f6e5afd2a1a7a44134bcf27ce8201776703a
Peter <18663039+petersatsuse@users.noreply.github.com>
2025-09-10 17:00:04 +0200

    adapted output.tf variables to new structure

--- output.tf
+++ output.tf
@@ -1,20 +1,20 @@
 output "instance_public_ip" {
-  description = "Public IP of the SUSE Micro instance"
-  value       = module.rke2_node.ec2_public_ip
+  description = "Public IP of the created VM (SUSE Micro) instance"
+  value       = module.cloud.public_ip
 }
 
-output "kubeconfig_path" {
-  description = "Path to the kubeconfig file"
-  value       = local_file.kube_config_yaml.filename
+output "kubeconfig_file_location" {
+  description = "Path to the generated Kubeconfig file on your local machine."
+  value       = length(local_file.kube_config_yaml) > 0 ? local_file.kube_config_yaml[0].filename : "Kubeconfig not generated."
 }
 
 output "next_steps" {
   description = "Follow these steps for accessing SUSE AI"
   value       = <<EOT
-  To access SUSE AI WebUI interface through your web browser, 
+  To access SUSE AI WebUI interface through your web browser,
   add below entry in your system's /etc/hosts file:
-  
-  <PUBLIC_IP_OF_EC2_INSTANCE>  suse-ollama-webui 
+
+  <PUBLIC_IP_OF_EC2_INSTANCE>  suse-ollama-webui
 
   And then access via https://suse-ollama-webui
 
